7814c8d3a29c612e9d733b3e56e4081996675b9e
BlueRedBlueYellow <74393018+BlueRedBlueYellow@users.noreply.github.com>
2023-06-11 04:53:11 -0500

    Generate grid, add style when each square is hovered over

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-const board = document.querySelector("#board");
-let boardSize = 16;
-for (let i = 0; i < boardSize; i++) {
-    let column = document.createElement("div");
-    column.classList.add("column");
-    board.appendChild(column);
+function addHoverEffect(e) {
+    e.target.classList.add("hovered");
+}
+
+function generateGrid(boardSize) {
+    const board = document.querySelector("#board");
     for (let i = 0; i < boardSize; i++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        column.appendChild(square);
+        let column = document.createElement("div");
+        column.classList.add("column");
+        board.appendChild(column);
+        for (let i = 0; i < boardSize; i++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.addEventListener("mouseover", addHoverEffect);
+            column.appendChild(square);
+        }
     }
 }
 
-
+generateGrid(16);
